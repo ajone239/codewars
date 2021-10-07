cf7381300928d4d202dd256faf5ac59a1b453a4e
Austin Jones <ajone239@vols.utk.edu>
2021-10-07 13:15:23 -0400

    yuh

--- src/codewars.rs
+++ src/codewars.rs
@@ -11,17 +11,24 @@ fn solution(word: &str, ending: &str) -> bool {
     let rword: Vec<char> = word.chars().rev().collect();
 
     for (i, c) in ending.chars().rev().enumerate() {
-        if c != rword[i] { return false; }
+        if c != rword[i] {
+            return false;
+        }
     }
     return true;
 }
 
 #[allow(dead_code)]
 fn high_and_low(numbers: &str) -> String {
-    let nums:Vec<i32> = numbers.split(' ').map(|s| {
-        s.parse::<i32>().unwrap()
-    }).collect();
-    format!("{} {}",nums.iter().max().unwrap(), nums.iter().min().unwrap())
+    let nums: Vec<i32> = numbers
+        .split(' ')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+    format!(
+        "{} {}",
+        nums.iter().max().unwrap(),
+        nums.iter().min().unwrap()
+    )
 }
 
 #[allow(dead_code)]
@@ -30,20 +37,21 @@ fn array_diff<T: PartialEq>(a: Vec<T>, b: Vec<T>) -> Vec<T> {
 }
 
 #[allow(dead_code)]
-fn duplicate_encode(word:&str)->String {
-
+fn duplicate_encode(word: &str) -> String {
     let word = word.to_lowercase();
 
     let mut set = std::collections::HashSet::new();
 
-    word.chars().map(|c| {
-        if word.matches(c).count() > 1 {
-            ')'
-        } else {
-            set.insert(c);
-            '('
-        }
-    }).collect::<String>()
+    word.chars()
+        .map(|c| {
+            if word.matches(c).count() > 1 {
+                ')'
+            } else {
+                set.insert(c);
+                '('
+            }
+        })
+        .collect::<String>()
 }
 
 #[allow(dead_code)]
@@ -79,9 +87,9 @@ fn tribonacci(signature: &[f64; 3], n: usize) -> Vec<f64> {
         rv.push(i.clone());
     }
     for i in 3..(n) {
-        rv.push(rv[i-3..i].into_iter().sum());
+        rv.push(rv[i - 3..i].into_iter().sum());
     }
-    rv.resize(n,0.);
+    rv.resize(n, 0.);
     return rv;
 }
 
@@ -118,7 +126,6 @@ fn persistence(num: u64) -> u64 {
         let mut temp = 1;
         while hold > 0 {
             temp *= hold % 10;
-            println!("{}", temp);
             hold /= 10;
         }
         hold = temp;
@@ -129,13 +136,14 @@ fn persistence(num: u64) -> u64 {
 
 #[allow(dead_code)]
 fn sum_pairs(ints: &[i8], s: i8) -> Option<(i8, i8)> {
-
     let mut low: usize = ints.len();
     let mut ret: Option<(i8, i8)> = None;
 
-    for (i,item) in ints.iter().enumerate() {
-        if i >= low { break; }
-        for (j, jtem) in ints[(i + 1)..low].iter().enumerate(){
+    for (i, item) in ints.iter().enumerate() {
+        if i >= low {
+            break;
+        }
+        for (j, jtem) in ints[(i + 1)..low].iter().enumerate() {
             if item + jtem == s {
                 ret = Some((*item, *jtem));
                 low = j + i + 1;
@@ -143,12 +151,128 @@ fn sum_pairs(ints: &[i8], s: i8) -> Option<(i8, i8)> {
             }
         }
     }
-    return ret;
+    ret
+}
+
+#[allow(dead_code)]
+fn song_decoder(song: &str) -> String {
+    song.split("WUB")
+        .map(|s| s.trim())
+        .filter(|&s| s != "")
+        .collect::<Vec<&str>>()
+        .join(" ")
+}
+
+#[allow(dead_code)]
+fn count_duplicates(text: &str) -> u32 {
+    use std::collections::HashMap;
+
+    let mut char_count = HashMap::new();
+
+    for c in text.to_lowercase().chars() {
+        match char_count.get(&c) {
+            Some(&val) => char_count.insert(c, val + 1),
+            None => char_count.insert(c, 1),
+        };
+    }
+    char_count.iter().filter(|&val| val.1 > &1).count() as u32
+}
+
+#[allow(dead_code)]
+fn solution_1(num: i32) -> i32 {
+    (3..num).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
+}
+
+/// Failed
+#[allow(dead_code)]
+fn remove_nb(m: i32) -> Vec<(i32, i32)> {
+    let sum = (m * (m + 1)) / 2;
+    let mut rv = Vec::new();
+    for i in 2..m {
+        let temp = sum - i;
+        for j in 2..m {
+            if temp % j == 0 && (temp - j) / j == i {
+                rv.push((i, j));
+                break;
+            }
+        }
+    }
+    rv
+}
+
+#[allow(dead_code)]
+fn sum_consecutives(numbers: &Vec<i32>) -> Vec<i32> {
+    let mut hold = numbers[0];
+    let mut sum = numbers[0];
+    let mut result = vec![];
+    for i in numbers[1..].iter() {
+        if i == &hold {
+            sum += i;
+        } else {
+            result.push(sum);
+            hold = *i;
+            sum = *i;
+        }
+    }
+    result.push(sum);
+    result
 }
 
 #[cfg(test)]
-mod tests{
+mod tests {
+
     use super::*;
+
+    #[test]
+    fn test_sample() {
+        println!("Sample Tests");
+
+        let input = vec![1, 4, 4, 4, 0, 4, 3, 3, 1];
+        let expected = vec![1, 12, 0, 4, 6, 1];
+        println!("Input: {:?}", input);
+        assert_eq!(sum_consecutives(&input), expected);
+
+        let input = vec![-5, -5, 7, 7, 12, 0];
+        let expected = vec![-10, 14, 12, 0];
+        println!("Input: {:?}", input);
+        assert_eq!(sum_consecutives(&input), expected);
+    }
+
+    fn testing(n: i32, exp: Vec<(i32, i32)>) -> () {
+        assert_eq!(remove_nb(n), exp)
+    }
+
+    #[test]
+    fn basics_remove_nb() {
+        testing(26, vec![(15, 21), (21, 15)]);
+        testing(100, vec![]);
+        testing(101, vec![(55, 91), (91, 55)]);
+        testing(102, vec![(70, 73), (73, 70)]);
+    }
+
+    #[test]
+    fn returns_expected_1() {
+        assert_eq!(solution_1(10), 23);
+        assert_eq!(solution_1(11), 33);
+        assert_eq!(solution_1(6), 8);
+    }
+
+    #[test]
+    fn test_abcde() {
+        assert_eq!(count_duplicates("abcde"), 0);
+        assert_eq!(count_duplicates("abcdea"), 1);
+        assert_eq!(count_duplicates("indivisibility"), 1);
+        assert_eq!(count_duplicates("aabcdea"), 1);
+        assert_eq!(count_duplicates("aabbdea"), 2);
+    }
+
+    #[test]
+    fn returns_expected() {
+        assert_eq!(song_decoder("WUBAWUBWUBC"), "A C");
+        assert_eq!(song_decoder("AWUBWUBWUBBWUBWUBWUBC"), "A B C");
+        assert_eq!(song_decoder("WUBAWUBBWUBCWUB"), "A B C");
+        assert_eq!(song_decoder("AWUBBWUBC"), "A B C");
+    }
 
     #[test]
     fn sum_pairs_test() {
@@ -190,31 +314,53 @@ mod tests{
 
     #[test]
     fn array_diff_test() {
-        assert_eq!(array_diff(vec![1,2], vec![1]), vec![2]);
-        assert_eq!(array_diff(vec![1,2,2], vec![1]), vec![2,2]);
-        assert_eq!(array_diff(vec![1,2,2], vec![2]), vec![1]);
-        assert_eq!(array_diff(vec![1,2,2], vec![]), vec![1,2,2]);
-        assert_eq!(array_diff(vec![], vec![1,2]), vec![]);
+        assert_eq!(array_diff(vec![1, 2], vec![1]), vec![2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![1]), vec![2, 2]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![2]), vec![1]);
+        assert_eq!(array_diff(vec![1, 2, 2], vec![]), vec![1, 2, 2]);
+        assert_eq!(array_diff(vec![], vec![1, 2]), vec![]);
     }
 
     #[test]
     fn duplicate_encode_tests() {
-        assert_eq!(duplicate_encode("din"),"(((");
-        assert_eq!(duplicate_encode("recede"),"()()()");
-        assert_eq!(duplicate_encode("Success"),")())())","should ignore case");
-        assert_eq!(duplicate_encode("(( @"),"))((");
+        assert_eq!(duplicate_encode("din"), "(((");
+        assert_eq!(duplicate_encode("recede"), "()()()");
+        assert_eq!(duplicate_encode("Success"), ")())())", "should ignore case");
+        assert_eq!(duplicate_encode("(( @"), "))((");
     }
 
     #[test]
     fn tribonacci_test() {
-        assert_eq!(tribonacci(&[0., 1., 1.], 10), vec![0., 1., 1., 2., 4., 7., 13., 24., 44., 81.]);
-        assert_eq!(tribonacci(&[1., 0., 0.], 10), vec![1., 0., 0., 1., 1., 2., 4., 7., 13., 24.]);
-        assert_eq!(tribonacci(&[0., 0., 0.], 10), vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]);
-        assert_eq!(tribonacci(&[1., 2., 3.], 10), vec![1., 2., 3., 6., 11., 20., 37., 68., 125., 230.]);
-        assert_eq!(tribonacci(&[3., 2., 1.], 10), vec![3., 2., 1., 6., 9., 16., 31., 56., 103., 190.]);
+        assert_eq!(
+            tribonacci(&[0., 1., 1.], 10),
+            vec![0., 1., 1., 2., 4., 7., 13., 24., 44., 81.]
+        );
+        assert_eq!(
+            tribonacci(&[1., 0., 0.], 10),
+            vec![1., 0., 0., 1., 1., 2., 4., 7., 13., 24.]
+        );
+        assert_eq!(
+            tribonacci(&[0., 0., 0.], 10),
+            vec![0., 0., 0., 0., 0., 0., 0., 0., 0., 0.]
+        );
+        assert_eq!(
+            tribonacci(&[1., 2., 3.], 10),
+            vec![1., 2., 3., 6., 11., 20., 37., 68., 125., 230.]
+        );
+        assert_eq!(
+            tribonacci(&[3., 2., 1.], 10),
+            vec![3., 2., 1., 6., 9., 16., 31., 56., 103., 190.]
+        );
         assert_eq!(tribonacci(&[1., 1., 1.], 1), vec![1.]);
         assert_eq!(tribonacci(&[300., 200., 100.], 0), vec![]);
-        assert_eq!(tribonacci(&[0.5, 0.5, 0.5], 30), vec![0.5, 0.5, 0.5, 1.5, 2.5, 4.5, 8.5, 15.5, 28.5, 52.5, 96.5, 177.5, 326.5, 600.5, 1104.5, 2031.5, 3736.5, 6872.5, 12640.5, 23249.5, 42762.5, 78652.5, 144664.5, 266079.5, 489396.5, 900140.5, 1655616.5, 3045153.5, 5600910.5, 10301680.5]);
+        assert_eq!(
+            tribonacci(&[0.5, 0.5, 0.5], 30),
+            vec![
+                0.5, 0.5, 0.5, 1.5, 2.5, 4.5, 8.5, 15.5, 28.5, 52.5, 96.5, 177.5, 326.5, 600.5,
+                1104.5, 2031.5, 3736.5, 6872.5, 12640.5, 23249.5, 42762.5, 78652.5, 144664.5,
+                266079.5, 489396.5, 900140.5, 1655616.5, 3045153.5, 5600910.5, 10301680.5
+            ]
+        );
     }
 
     #[test]
