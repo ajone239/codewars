1c5321fe565d055735a2f9b0d1c9f7f0fd0785fd
Austin Jones <ajone239@vols.utk.edu>
2023-10-23 16:49:02 -0400

    new tokenization logic

--- src/to_postfix.rs
+++ src/to_postfix.rs
@@ -1,19 +1,64 @@
 // Add your tests here.
 // See https://doc.rust-lang.org/stable/rust-by-example/testing/unit_testing.html
 
-use std::cell::RefCell;
 use std::fmt::Display;
-use std::{collections::VecDeque, rc::Rc};
 
 #[allow(dead_code)]
 fn to_postfix(_infix: &str) -> String {
-    let tree = Tree::new(_infix);
+    let _tokens = tokenize(_infix);
 
-    let post_fix = tree.preorder_print();
-
-    post_fix
+    unimplemented!()
 }
 
+fn tokenize(expr: &str) -> Vec<Token<'_>> {
+    let mut tokens = Vec::new();
+
+    let mut char_iter = expr.chars().enumerate();
+
+    while let Some((i, c)) = char_iter.next() {
+        match c {
+            '0'..='9' => {
+                tokens.push(Token::Number(c.to_digit(10).unwrap() as i32));
+            }
+            '+' | '-' | '*' | '/' | '^' => {
+                tokens.push(Token::Operator(Operator::new(c)));
+            }
+            '(' => {
+                let closing_paren = find_closing_paren(&mut char_iter);
+                tokens.push(Token::ParenStatement(&expr[i + 1..closing_paren]));
+            }
+            ')' => {
+                unreachable!()
+            }
+            _ => panic!(),
+        }
+    }
+    tokens
+}
+
+fn find_closing_paren(char_iter: &mut std::iter::Enumerate<std::str::Chars>) -> usize {
+    let mut count = 1;
+    while let Some((i, c)) = char_iter.next() {
+        match c {
+            '(' => count += 1,
+            ')' => count -= 1,
+            _ => (),
+        }
+        if count == 0 {
+            return i;
+        }
+    }
+    unreachable!()
+}
+
+#[derive(Debug, PartialEq)]
+enum Token<'a> {
+    Number(i32),
+    Operator(Operator),
+    ParenStatement(&'a str),
+}
+
+#[derive(Debug, PartialEq)]
 enum Operator {
     Add,
     Subtract,
@@ -49,89 +94,59 @@ impl Display for Operator {
     }
 }
 
-type Node<'a> = Rc<RefCell<NodeData<'a>>>;
-
-enum NodeData<'a> {
-    Number(i32),
-    Op {
-        op: Operator,
-        left: Node<'a>,
-        right: Node<'a>,
-    },
-    Unprocessed(&'a str),
-}
-
-impl NodeData<'_> {
-    fn preorder_print(&self) -> String {
-        match self {
-            NodeData::Number(n) => format!("{}", n),
-            NodeData::Op { op, left, right } => {
-                format!(
-                    "{}{}{}",
-                    left.borrow().preorder_print(),
-                    right.borrow().preorder_print(),
-                    op
-                )
-            }
-            NodeData::Unprocessed(expr) => expr.to_string(),
-        }
-    }
-}
-
-fn process<'a>(node: Node<'a>, unprocessed: &mut VecDeque<Node<'a>>) {
-    let expr = match *node.borrow() {
-        NodeData::Unprocessed(expr) => expr,
-        _ => panic!("This is already processed"),
-    };
-
-    for ops in EMDAS.iter().rev() {
-        for (i, c) in expr.chars().enumerate() {
-            if ops.contains(c) {
-                let op = Operator::new(c);
-                let left = Rc::new(RefCell::new(NodeData::Unprocessed(&expr[..i])));
-                let right = Rc::new(RefCell::new(NodeData::Unprocessed(&expr[i + 1..])));
-
-                unprocessed.push_back(left.clone());
-                unprocessed.push_back(right.clone());
-
-                *node.borrow_mut() = NodeData::Op { op, left, right };
-
-                return;
-            }
-        }
-    }
-    if "1234567890".contains(&expr) {
-        *node.borrow_mut() = NodeData::Number(expr.parse().unwrap());
-    }
-}
-
-struct Tree<'a> {
-    root: Node<'a>,
-}
-
-impl<'a> Tree<'a> {
-    fn new(expression: &'a str) -> Self {
-        let mut tree = Tree {
-            root: Rc::new(RefCell::new(NodeData::Unprocessed(expression))),
-        };
-
-        let mut unprocessed = VecDeque::new();
-        unprocessed.push_back(tree.root.clone());
-
-        while let Some(node) = unprocessed.pop_front() {
-            process(node, &mut unprocessed);
-        }
-
-        tree
-    }
-    fn preorder_print(&self) -> String {
-        self.root.borrow().preorder_print()
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use super::to_postfix;
+    use super::*;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case::simple("()", 0, 1)]
+    #[case::jump("(())", 0, 3)]
+    #[case::double_jump("(()())", 0, 5)]
+    #[case::inner("(())", 1, 2)]
+    #[case::data("(12+45)", 0, 6)]
+    fn test_find_closing_paren(#[case] expr: &str, #[case] i: usize, #[case] expected: usize) {
+        let mut char_iter = expr.chars().enumerate();
+
+        for _ in 0..(i + 1) {
+            char_iter.next();
+        }
+
+        let actual = find_closing_paren(&mut char_iter);
+
+        assert_eq!(actual, expected);
+
+        if let Some((i, _)) = char_iter.next() {
+            assert_eq!(i, actual + 1);
+        }
+    }
+
+    #[rstest]
+    #[case(
+            "2+7*5",
+            vec![
+                Token::Number(2),
+                Token::Operator(Operator::new('+')),
+                Token::Number(7),
+                Token::Operator(Operator::new('*')),
+                Token::Number(5),
+            ]
+        )
+    ]
+    #[case(
+            "3*3/(7+1)",
+            vec![
+                Token::Number(3),
+                Token::Operator(Operator::new('*')),
+                Token::Number(3),
+                Token::Operator(Operator::new('/')),
+                Token::ParenStatement("7+1"),
+            ]
+        )
+    ]
+    fn test_tokenize(#[case] expr: &str, #[case] expected: Vec<Token>) {
+        assert_eq!(tokenize(expr), expected);
+    }
 
     fn do_test(actual: &str, expected: &str) {
         assert_eq!(
@@ -140,17 +155,19 @@ mod tests {
         )
     }
 
-    #[test]
-    fn test_simple() {
-        do_test(&to_postfix("2+7*5"), "275*+");
-        do_test(&to_postfix("1^2^3"), "123^^");
-        do_test(&to_postfix("1+2*3/4-5"), "1234/*5-+");
+    #[rstest]
+    #[case("2+7*5", "275*+")]
+    #[case("1^2^3", "123^^")]
+    #[case("1+2*3/4-5", "1234/*5-+")]
+    fn test_simple(#[case] expr: &str, #[case] expected: &str) {
+        do_test(&to_postfix(expr), expected);
     }
 
-    #[test]
-    fn fixed_tests() {
-        do_test(&to_postfix("3*3/(7+1)"), "33*71+/");
-        do_test(&to_postfix("5+(6-2)*9+3^(7-1)"), "562-9*+371-^+");
-        do_test(&to_postfix("(5-4-1)+9/5/2-7/1/7"), "54-1-95/2/+71/7/-");
+    #[rstest]
+    #[case("3*3/(7+1)", "33*71+/")]
+    #[case("5+(6-2)*9+3^(7-1)", "562-9*+371-^+")]
+    #[case("(5-4-1)+9/5/2-7/1/7", "54-1-95/2/+71/7/-")]
+    fn fixed_tests(#[case] expr: &str, #[case] expected: &str) {
+        do_test(&to_postfix(expr), expected);
     }
 }
