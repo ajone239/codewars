159ef354eff60c1e424e61045d714c51be5b5ffd
Austin Jones <ajone239@vols.utk.edu>
2023-10-23 10:37:32 -0400

    cool

--- src/to_postfix.rs
+++ src/to_postfix.rs
@@ -143,15 +143,14 @@ mod tests {
     #[test]
     fn test_simple() {
         do_test(&to_postfix("2+7*5"), "275*+");
+        do_test(&to_postfix("1^2^3"), "123^^");
         do_test(&to_postfix("1+2*3/4-5"), "1234/*5-+");
     }
 
     #[test]
     fn fixed_tests() {
-        do_test(&to_postfix("2+7*5"), "275*+");
         do_test(&to_postfix("3*3/(7+1)"), "33*71+/");
         do_test(&to_postfix("5+(6-2)*9+3^(7-1)"), "562-9*+371-^+");
         do_test(&to_postfix("(5-4-1)+9/5/2-7/1/7"), "54-1-95/2/+71/7/-");
-        do_test(&to_postfix("1^2^3"), "123^^");
     }
 }
