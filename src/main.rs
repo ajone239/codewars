5456e843b8d856004dda2be590e34df1926a3e6a
Austin Jones <ajone239@vols.utk.edu>
2023-10-29 23:49:25 -0400

    done with polynomial multiplicatioin

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-use codewars::to_postfix::to_postfix;
-
 fn main() {
-    let expr = "5+(6-2)*9+3^(7-1)";
-
-    let postfix = to_postfix(&expr);
-    println!("{:?}", postfix);
+    println!("hello world");
 }
