7b2d75913914bfe496083189b04cd5809a1cf01c
Austin Jones <ajone239@vols.utk.edu>
2023-11-05 12:52:58 -0500

    working

--- src/path_finder/path_finder_1.rs
+++ src/path_finder/path_finder_1.rs
@@ -15,9 +15,168 @@
 //! #4: where are you?
 //! #5: there's someone here
 
+use std::collections::VecDeque;
+use std::fmt::Debug;
+
 #[allow(dead_code)]
-fn path_finder(_maze: &str) -> bool {
-    unimplemented!()
+pub fn path_finder(maze: &str) -> bool {
+    let mut maze = Maze::new(maze);
+
+    if maze.is_finished(0, 0) {
+        return true;
+    }
+
+    let mut flood = Flood::new(&mut maze);
+
+    loop {
+        println!("{:?}", flood.maze);
+        println!();
+        match flood.flood_step() {
+            FloodStep::Continue => (),
+            FloodStep::Failed => return false,
+            FloodStep::Finished => return true,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum FloodStep {
+    Continue,
+    Failed,
+    Finished,
+}
+
+#[derive(Debug)]
+struct Flood<'a> {
+    maze: &'a mut Maze,
+    edges: VecDeque<(usize, usize)>,
+}
+
+impl<'a> Flood<'a> {
+    fn new(maze: &'a mut Maze) -> Self {
+        let mut edges = VecDeque::new();
+
+        edges.push_back((0, 0));
+
+        Self { maze, edges }
+    }
+
+    fn flood_step(&mut self) -> FloodStep {
+        let (x, y) = match self.edges.pop_front() {
+            Some((x, y)) => (x, y),
+            None => {
+                return FloodStep::Failed;
+            }
+        };
+        // north, east, south, west
+        let directions: [(i64, i64); 4] = [(-1, 0), (0, 1), (1, 0), (0, -1)];
+        let directions = directions
+            .iter()
+            .filter(|(i, j)| {
+                self.maze
+                    .legal_square((x as i64 + i) as usize, (y as i64 + j) as usize)
+            })
+            .map(|(i, j)| ((x as i64 + i) as usize, (y as i64 + j) as usize))
+            .collect::<Vec<_>>();
+
+        for (i, j) in directions {
+            if self.maze.is_finished(i, j) {
+                return FloodStep::Finished;
+            }
+            if self.maze.try_set_seen(i, j) {
+                self.edges.push_back((i, j));
+            }
+        }
+
+        FloodStep::Continue
+    }
+}
+
+#[derive(PartialEq)]
+enum Tile {
+    Empty,
+    Seen,
+    Wall,
+    Finished,
+}
+
+impl Debug for Tile {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Tile::Empty => write!(f, "."),
+            Tile::Seen => write!(f, "S"),
+            Tile::Wall => write!(f, "W"),
+            Tile::Finished => write!(f, "F"),
+        }
+    }
+}
+
+struct Maze {
+    tiles: Vec<Vec<Tile>>,
+    width: usize,
+    height: usize,
+}
+
+impl Maze {
+    fn new(maze: &str) -> Self {
+        let mut tiles: Vec<Vec<Tile>> = maze
+            .lines()
+            .map(|line| {
+                line.chars()
+                    .map(|c| match c {
+                        '.' => Tile::Empty,
+                        'W' => Tile::Wall,
+                        _ => unreachable!(),
+                    })
+                    .collect()
+            })
+            .collect();
+
+        let width = tiles[0].len();
+        let height = tiles.len();
+
+        tiles[0][0] = Tile::Seen;
+        tiles[width - 1][height - 1] = Tile::Finished;
+
+        Self {
+            tiles,
+            width,
+            height,
+        }
+    }
+
+    fn try_set_seen(&mut self, x: usize, y: usize) -> bool {
+        if !self.legal_square(x, y) {
+            return false;
+        }
+        self.tiles[y][x] = Tile::Seen;
+        true
+    }
+
+    fn legal_square(&self, x: usize, y: usize) -> bool {
+        if x >= self.width || y >= self.height {
+            return false;
+        }
+        self.tiles[y][x] == Tile::Empty || self.tiles[y][x] == Tile::Finished
+    }
+
+    fn is_finished(&self, x: usize, y: usize) -> bool {
+        self.tiles[y][x] == Tile::Finished
+    }
+}
+
+impl Debug for Maze {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "Maze:")?;
+        for row in &self.tiles {
+            write!(f, "[")?;
+            for tile in row {
+                write!(f, "{:?}", tile)?;
+            }
+            writeln!(f, "]")?;
+        }
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -56,6 +215,7 @@ mod tests {
         ",
         false
     )]
+    #[case(".", true)]
     fn basic(#[case] maze: &str, #[case] expect: bool) {
         test_maze(maze, expect);
     }
